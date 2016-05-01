3eb529dda1a83df0d04d00b962dc5af4a47fe4ec
shuai peng <pengshuai2010@gmail.com>
2016-04-30 22:41:39 -0600

    finish make appointment

--- medical_system/medical_system/manage_appointment.py
+++ medical_system/medical_system/manage_appointment.py
@@ -1,3 +1,6 @@
+import calendar
+import datetime
+
 from medical_system.database import get_connection
 from medical_system.manage_patient import select_a_doctor
 
@@ -5,14 +8,84 @@ from medical_system.manage_patient import select_a_doctor
 def make_appointment():
     # TODO identify patient; choose doctor; show availability; choose availability; remove from doctor's availability; create appointment;
     # TODO insert appointment
+    # identify patient
+    print 'who is the patient?'
+    patient = select_a_patient()
     print 'which doctor do you want to schedule an appointment with?'
     doctor = select_a_doctor()
     selected_availability = select_a_timeslot(doctor)
-    create_appointment()
+    appointment = create_appointment(patient, selected_availability)
+    insert_appointment(appointment)
 
 
-def create_appointment():
-    pass
+def create_appointment(patient, availability):
+    appointment = {}
+    appointment['MedicalOfficeId'] = availability['MedicalOfficeId']
+    appointment['Doctor_Id'] = availability['Doctor_Id']
+    appointment['PatientID'] = patient['Person_Id']
+    body_temperature = float(raw_input("patient's body temperature is(Celsius):   "))
+    appointment['Body Temperature'] = body_temperature
+    weight = float(raw_input("patient's weight is(kg):   "))
+    appointment['Weight'] = weight
+    height = float(raw_input("patient's height is(meter):   "))
+    appointment['Height'] = height
+    appointment['Appointment Time'] = availability['From_Time']
+    appointment['Appointment Date'] = next_weekday(datetime.date.today(),
+                                                   list(calendar.day_abbr).index(availability['Day_Of_Week']))
+    return appointment
+
+
+def insert_appointment(appointment):
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    query = (
+        "INSERT INTO Appointment (MedicalOfficeId, Doctor_Id, PatientID, `Body Temperature`, Weight, Height, `Appointment Date`, `Appointment Time`) "
+        "VALUES (%(MedicalOfficeId)s, %(Doctor_Id)s, %(PatientID)s, %(Body Temperature)s, %(Weight)s, %(Height)s, %(Appointment Date)s, %(Appointment Time)s)")
+    cursor.execute(query, appointment)
+    # query = (
+    #     "INSERT INTO Appointment (MedicalOfficeId, Doctor_Id, PatientID, `Body Temperature`, Weight, Height, `Appointment Date`, `Appointment Time`) "
+    #     "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+    # tmp = (appointment['MedicalOfficeId'], appointment['Doctor_Id'], appointment['PatientID'], appointment['Body Temperature'], appointment['Weight'], appointment['Height'], appointment['Appointment Date'], appointment['Appointment Time'])
+    # cursor.execute(query, tmp)
+    appointment['Appointment_Id'] = cursor.lastrowid
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+
+def next_weekday(d, weekday):
+    ''' usage: d = datetime.date(2011, 7, 2)
+    next_monday = next_weekday(d, 0) # 0 = Monday, 1=Tuesday, 2=Wednesday...
+    print(next_monday)'''
+    days_ahead = weekday - d.weekday()
+    if days_ahead <= 0:  # Target day already happened this week
+        days_ahead += 7
+    return d + datetime.timedelta(days_ahead)
+
+
+def select_a_patient():
+    ''':returns patient '''
+    patient_list = get_patient_list()
+    for counter, patient in enumerate(patient_list):
+        print 'index number ', counter, patient
+    selection = int(raw_input('please select a patient by index number:   '))
+    return patient_list[selection]
+
+
+def get_patient_list():
+    patient_list = []
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    query = ("SELECT Other.Person_Id, First_Name, Last_Name, Date_Of_Birth, Deceased, Primary_Physician FROM Other "
+             "JOIN Person ON Person.Person_Id = Other.Person_Id")
+    cursor.execute(query)
+    for (person_id, first_name, last_name, date_of_birth, deceased, primary_physician) in cursor:
+        patient_list.append(
+            {'Person_Id': person_id, 'First_Name': first_name, 'Last_Name': last_name, 'Date_Of_Birth': date_of_birth,
+             'Deceased': deceased, 'Primary_Physician': primary_physician})
+    cursor.close()
+    cnx.close()
+    return patient_list
 
 
 def select_a_timeslot(doctor):
@@ -21,7 +94,7 @@ def select_a_timeslot(doctor):
     cnx = get_connection()
     cursor = cnx.cursor()
     query = (
-        "SELECT Doctor_Availability_Id, Day_Of_Week, From_Time, To_Time, Organization.Name, MedicalOfficeId, Doctor_Id FROM Doctor_Availability "
+        "SELECT Doctor_Availability_Id, Day_Of_Week, From_Time, To_Time, Organization.Name, Medical_Office.MedicalOfficeId, Doctor_Availability.Doctor_Id FROM Doctor_Availability "
         "JOIN Affiliation ON Affiliation.Doctor_Id = Doctor_Availability.Doctor_Id "
         "JOIN Medical_Office ON Medical_Office.MedicalOfficeId = Affiliation.MedicalOfficeId "
         "JOIN Organization ON Organization.Org_Id = Medical_Office.MedicalOfficeId "
