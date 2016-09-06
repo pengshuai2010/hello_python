f0e78aa37dcf24ce203b47881655bfdd66b45ec3
shuaipeng <pengshuai2010@gmail.com>
2016-09-06 10:07:02 -0600

    update parallel speed up figure

--- hello_python/drawing/parallelSpeedUpData.py
+++ hello_python/drawing/parallelSpeedUpData.py
@@ -8,14 +8,9 @@ time = {"K1-N16-Inter": [11.9295, 6.4122, 3.1489, 1.697, 0.8873, 0.7202, 0.6016]
         "K1-N128-Inter": [10.4002, 6.5947, 4.9964, 3.7421, 3.7764, 3.7123, 4.8355],
         "K19-N128-Inter": [10.7097, 5.5312, 2.8784, 1.615, 0.8483, 0.6911, 0.5846]}
 
-# line, = plt.plot(threads, time["K1-N16-Inter"])
-# plt.plot(threads, time["K1-N16-Inter"],
-#                  threads, time["K7-N16-Inter"],
-#                  threads, time["K1-N128-Inter"],
-#                  threads, time["K19-N128-Inter"])
 plt.figure(1)
 for key in time.keys():
-    plt.plot(threads, time[key], label=key)
+    plt.loglog(threads, time[key], label=key)
 plt.xlabel('threads')
 plt.ylabel('seconds')
 plt.grid(True)
@@ -24,11 +19,12 @@ plt.title('execution time')
 
 plt.figure(2)
 for key in time.keys():
-    plt.loglog(threads, time[key], label=key)
+    plt.plot(threads, [time[key][0] / t for t in time[key]], label=key)
 plt.xlabel('threads')
-plt.ylabel('seconds')
+plt.ylabel('speedup')
 plt.grid(True)
-plt.legend()
+plt.legend(bbox_to_anchor=(0.5, 0.8),
+           bbox_transform=plt.gcf().transFigure)
 plt.title('speedup')
 
 plt.show()
