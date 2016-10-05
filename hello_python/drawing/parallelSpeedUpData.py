ad2c6ca962de9d3d04659cfde1e6d2ed5bdb80e8
shuai peng <pengshuai2010@gmail.com>
2016-10-05 09:38:21 -0600

    add speedup calculation

--- hello_python/drawing/parallelSpeedUpData.py
+++ hello_python/drawing/parallelSpeedUpData.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 ax = plt.subplot(111)
 
 threads = [1, 2, 4, 8, 16, 32]
-time = {"n=1000000000": [16.79, 8.44, 4.26, 2.18, 1.11, 0.6],
+time = {"n=1000000000": [16.79,  8.44, 4.26, 2.18, 1.11, 0.6],
         "n=2000000000": [33.58, 16.88, 8.5, 4.28, 2.18, 1.14],
         "n=4000000000": [66.96, 33.71, 16.95, 8.46, 4.29, 2.27],
         "n=8000000000": [134.26, 67.48, 33.97, 16.99, 8.58, 4.42]}
@@ -36,5 +36,8 @@ plt.grid(True)
 plt.legend(bbox_to_anchor=(0.5, 0.5),
            bbox_transform=plt.gcf().transFigure)
 plt.title('efficiency')
+for key in time.keys():
+    a = [time[key][0] / t for t in time[key]]
+    print ["{0:0.2f}".format(i) for i in a]
 
 plt.show()
